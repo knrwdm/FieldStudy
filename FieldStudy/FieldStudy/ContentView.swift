37f1d9d14c544fefb3199bb56a869f2f5283d931
Kayla Reap <knrwdm@mail.missouri.edu>
2019-11-22 22:11:11 -0600

    added table control

---
+++ FieldStudy/FieldStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FieldStudy
+//
+//  Created by Kayla Reap on 11/22/19.
+//  Copyright © 2019 Kayla Reap. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
